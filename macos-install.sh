3d5508a0e009f3537386b981e57f16e90d5ff279
Shaung Cheng <shaungc@umich.edu>
2018-09-02 20:04:01 -0400

    add git setup

--- macos-install.sh
+++ macos-install.sh
@@ -1,26 +1,42 @@
 
-# install brew. See https://brew.sh
+# config git
+#
+##### 
 
+echo "INFO: setting git..."
+
+# Set git to use the osxkeychain credential helper
+git config --global credential.helper osxkeychain
+
+# Set merge tool to vscode
+git config --global merge.tool vscode
+git config --global mergetool.vscode.cmd "code --wait $MERGED"
+git config --global diff.tool vscode
+git config --global difftool.vscode.cmd "code --wait --diff $LOCAL $REMOTE"
+
+# Setup user name and email
+git config --global user.name "Shaung Cheng"
+git config --global user.email shaungc@umich.edu
+
+
+
+
+
+# software installation
+#
+#####
+
+# install brew. See https://brew.sh
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-
-
 # GUI
-
 brew tap caskroom/cask
-
 brew cask install google-chrome visual-studio-code google-drive-file-stream wechat
 
-
-
 # optional GUI
-
 brew cask install slack
 
-
-
 # CLI
-
 # for now, install just python 3.6 instead of 3.7
 # brew install python3 
 brew install \
