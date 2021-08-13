975ee4af27039f9592f48d12d024f0461815fd93
Shaung Cheng <shaungc@umich.edu>
2021-08-13 15:10:11 -0700

    Another attempt to automate sudo (home-brew install script does not allow running as sudo)

--- macos-install.sh
+++ macos-install.sh
@@ -74,7 +74,7 @@ git config --global user.email ${USER_EMAIL}
 # the last "</dev/null" is to silent install prompt, see https://stackoverflow.com/a/25535532
 printf "INFO: installing brew..."
 export CI=1
-echo ${PASSWORD} | sudo -S /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo ${PASSWORD} | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # pyenv is config in zsh script (beautify-terminal)
 brew install pyenv
