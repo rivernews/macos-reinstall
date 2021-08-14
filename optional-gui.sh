47968d0eec6fa73724ac96482849b1f7371a59be
Shaung Cheng <shaungc@umich.edu>
2021-08-14 14:04:29 -0700

    Add sudo password automation

--- optional-gui.sh
+++ optional-gui.sh
@@ -1,8 +1,13 @@
 # GUIs
 
-brew install --cask slack google-drive
+if [ "${PASSWORD}" == "" ]; then
+   echo "Please provide your sudo password to install GUI apps:"
+   read PASSWORD
+fi
+
+echo ${PASSWORD} | brew install --cask slack google-drive
 # Pin Sketch version 72.4 to use it free
-brew install --cask https://github.com/Homebrew/homebrew-cask/blob/bf85486d8a5aa60cdc0622809e81d61ea43841ff/Casks/sketch.rb
+echo ${PASSWORD} | brew install --cask https://github.com/Homebrew/homebrew-cask/blob/bf85486d8a5aa60cdc0622809e81d61ea43841ff/Casks/sketch.rb
 
 # M1 not supported
 # brew install --cask skype docker
