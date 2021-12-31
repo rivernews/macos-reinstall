bc833ce4422dc8b9a37e6275367df1ea545a8120
Shaung <shaungc@umich.edu>
2021-12-30 23:53:47 -0800

    move and update mvn

--- optional-cli.sh
+++ optional-cli.sh
@@ -8,3 +8,7 @@ brew install kubernetes-cli
 brew install terraform
 
 # npm install -g @angular/cli
+
+# Install NVM
+# please refer to the nvm repo for latest version: https://github.com/nvm-sh/nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
