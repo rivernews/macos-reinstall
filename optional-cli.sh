392bf498a86f21995c7633d7e0127a5528804e12
River News <shaungc@umich.edu>
2022-03-25 23:14:50 -0700

    Update optional-cli.sh

--- optional-cli.sh
+++ optional-cli.sh
@@ -3,13 +3,17 @@ brew install postgresql
 
 # brew install doctl
 
-brew install kubernetes-cli
+# brew install kubernetes-cli
 
-brew install terraform
+# brew install terraform
 
 # npm install -g @angular/cli
 
-# Install NVM
-# please refer to the nvm repo for latest version: https://github.com/nvm-sh/nvm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-# nvm install node # install latest node
+cat << EOF
+Install NVM
+Run below command in a separate console, then continue. Please refer to the nvm repo for latest version: https://github.com/nvm-sh/nvm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node # install latest node
+EOF
+read -s _
